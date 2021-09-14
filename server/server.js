3cdeaae038df6c1b5a52bc7c8fc293a97f816da7
Kateryna B <butsneva.ekaterina@gmail.com>
2021-09-14 17:49:00 -0500

    server side fixes

--- server/server.js
+++ server/server.js
@@ -1,44 +1,41 @@
-require('dotenv').config();
-const path = require('path');
 const express = require('express');
-//import apollo server
-const { ApolloServer } = require('apollo-server-express');
-// const routes = require('./routes');
-const { authMiddleware } = require('./utils/auth');
-
-// import our typDefs and resolvers
-const { typeDefs, resolvers } = require('./schemas');
+const path = require('path');
 const db = require('./config/connection');
+const routes = require('./routes');
+const { authMiddleware } = require('./utils/auth');
+const { ApolloServer } = require('apollo-server-express');
+const { typeDefs, resolvers } = require('./schemas');
 
-const PORT = process.env.PORT || 3001;
 const app = express();
+const PORT = process.env.PORT || 3001;
 
-// create a new apollo server and pass in our schema data
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-  context: authMiddleware,
-});
+async function startApolloServer() {
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    context: authMiddleware,
+  });
+  await server.start();
 
-// integrate the apollo server with express application middleware
-server.applyMiddleware({ app });
+  server.applyMiddleware({ app });
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+  app.use(express.urlencoded({ extended: false }));
+  app.use(express.json());
 
-// if we're in production, serve client/build as static assets
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
-}
+  if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/build')));
+  }
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/build/index.html'));
+  });
+  // app.use(routes);
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'));
-});
-
-db.once('open', () => {
-  app.listen(PORT, () => {
-    console.log(`API server running on port ${PORT}!`);
-    //log where we can go test our GQL API
+  db.once('open', () => {
+    app.listen(PORT, () =>
+      console.log(`🌍 Now listening on localhost:${PORT}`)
+    );
     console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
   });
-});
+}
+
+startApolloServer();
--- server/utils/auth.js
+++ server/utils/auth.js
@@ -4,38 +4,31 @@ const secret = 'mysecretsshhhhh';
 const expiration = '2h';
 
 module.exports = {
-  signToken: function ({ username, email, _id }) {
-    const payload = { username, email, _id };
-
-    return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
-  },
-
   authMiddleware: function ({ req }) {
-    //allows token to be sent via req.body, req.query, or headers
-
+    // allows token to be sent via req.body, req.query, or headers
     let token = req.body.token || req.query.token || req.headers.authorization;
-    // let token = req.query.token || req.headers.authorization;
 
     //separate "Bearer" from "<tokenvalue>"
     if (req.headers.authorization) {
       token = token.split(' ').pop().trim();
     }
 
-    //if no token, return request object as is
     if (!token) {
-      console.log('We Got here!!', token);
       return req;
     }
 
     try {
-      //decode and attach user data to request object
       const { data } = jwt.verify(token, secret, { maxAge: expiration });
       req.user = data;
-    } catch (err) {
-      console.log(err, 'Invalid token');
+    } catch {
+      console.log('Invalid token');
     }
 
-    //return updated request object
     return req;
+  },
+  signToken: function ({ username, email, _id }) {
+    const payload = { username, email, _id };
+
+    return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
   },
 };
